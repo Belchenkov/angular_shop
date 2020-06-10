5d891c4f78dc9a685110aa03981b4a50829826b2
Aleksey Belchenkov <u608110@gmail.com>
2020-06-10 10:40:12 +0300

    Create Product Service & Send POST for create product

--- src/app/admin/add-page/add-page.component.ts
+++ src/app/admin/add-page/add-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from "@angular/forms";
 
+import { ProductService } from "../../shared/product.service";
+
 @Component({
   selector: 'app-add-page',
   templateUrl: './add-page.component.html',
@@ -10,7 +12,9 @@ export class AddPageComponent implements OnInit {
   form: FormGroup;
   submitted = false;
 
-  constructor() { }
+  constructor(
+    private productService: ProductService
+  ) { }
 
   ngOnInit() {
     this.form = new FormGroup({
@@ -33,9 +37,12 @@ export class AddPageComponent implements OnInit {
       photo: this.form.value.photo,
       info: this.form.value.info,
       price: this.form.value.price,
+      date: new Date(),
     };
 
-
+    this.productService.create(product).subscribe(res => {
+      console.log(res);
+    });
   }
 
 }
